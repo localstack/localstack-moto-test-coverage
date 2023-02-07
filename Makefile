83e9ecfaba7b3324567d6b78892c4320e48a092b
Stefanie Plieschnegger <stefanie.plieschnegger@localstack.cloud>
2023-02-07 11:19:44 +0100

    add pre-commit hook for formatting

--- Makefile
+++ Makefile
@@ -17,27 +17,36 @@ $(VENV_ACTIVATE):
 
 venv: $(VENV_ACTIVATE)    ## Create a new (empty) virtual environment
 
-checkout_moto:
+checkout-moto:
 	test -d moto || git clone https://github.com/getmoto/moto.git
 
-update_moto: venv
+update-moto: venv checkout-moto
 	cd moto && git checkout master && git fetch origin master
 	$(VENV_RUN); cd moto && make init
 
-init_extension: venv
+init-extension: venv
 	# TODO user must be logged in already
-	pip install localstack
 	cd collect-raw-metric-data-extension && make install
 	localstack extensions init
 	localstack extensions dev enable ./collect-raw-metric-data-extension
 
-install: venv checkout_moto init_extension
+init-precommit: venv
+	($(VENV_RUN); pre-commit install)
+
+install-dependencies: venv
+	$(VENV_RUN); pip install -r requirements.txt
+
+install: venv install-dependencies init-precommit update-moto init-extension
 	$(VENV_RUN); $(PIP_CMD) install pytest requests
 	$(VENV_RUN); $(PIP_CMD) install pytest requests
 
 run-tests:
 	cp conftest.py moto/tests/
 	$(VENV_RUN); python -m pytest --capture=no --junitxml=target/reports/pytest.xml  moto --tb=line
+
+format:
+	($(VENV_RUN); python -m isort conftest.py; python -m black conftest.py )
+	($(VENV_RUN); python -m isort collect-raw-metric-data-extension; python -m black collect-raw-metric-data-extension )
 
 clean:
 	rm -rf moto
--- conftest.py
+++ conftest.py
@@ -4,33 +4,33 @@ import os
 from pathlib import Path
 from typing import Iterator
 from unittest.mock import patch
+
 import boto3 as boto3
-from botocore.config import Config
 import pytest as pytest
 import requests as requests
+from botocore.config import Config
 from requests.adapters import HTTPAdapter, Retry
+from tests import DEFAULT_ACCOUNT_ID
 
 from moto import settings
 from moto.core.models import BaseMockAWS
-from tests import DEFAULT_ACCOUNT_ID
 
 BASE_PATH = os.path.join(os.path.dirname(__file__), "../../target/reports")
 
-FNAME_RAW_DATA_CSV = os.path.join(BASE_PATH,"metric_data_raw.csv")
-
+FNAME_RAW_DATA_CSV = os.path.join(BASE_PATH, "metric_data_raw.csv")
 
 # overriding ENVs + settings to use LocalStack as endpoint
-os.environ["TEST_SERVER_MODE_ENDPOINT"] = 'http://localhost:4566'
-os.environ["MOTO_CALL_RESET_API"] = 'false'
+os.environ["TEST_SERVER_MODE_ENDPOINT"] = "http://localhost:4566"
+os.environ["MOTO_CALL_RESET_API"] = "false"
 settings.TEST_SERVER_MODE = True
+
 
 @pytest.fixture(scope="session", autouse=True)
 def default_localstack_client_config_fixture() -> Iterator[None]:
     def localstack_client(*args, org=boto3.client, **kwargs):
         # kwargs["endpoint_url"] = 'http://localhost:4566' # TODO could also use fixture to override endpoint_url
         config: Config = kwargs.get("config")
-        max_retry_config = Config(
-            retries={"max_attempts": 1, "total_max_attempts": 2})
+        max_retry_config = Config(retries={"max_attempts": 1, "total_max_attempts": 2})
         if config:
             config.merge(max_retry_config)
         else:
@@ -53,8 +53,11 @@ def default_localstack_client_fixture() -> Iterator[None]:
         self.FAKE_KEYS["AWS_ACCESS_KEY_ID"] = DEFAULT_ACCOUNT_ID
         org(self)
 
-    with patch("moto.core.models.BaseMockAWS.mock_env_variables", autospec=True,
-               side_effect=localstack_mock_env_variables):
+    with patch(
+        "moto.core.models.BaseMockAWS.mock_env_variables",
+        autospec=True,
+        side_effect=localstack_mock_env_variables,
+    ):
         yield
 
 
@@ -66,22 +69,19 @@ def default_cleanup_localstack_resources():
     the endpoint /_pods/state/reset
     """
     yield
-    payload = {'persistence': True}  # defining no services means all services will be cleared (we don't know which interservice communication tests trigger)
-    headers = {'content-type': 'application/json'}
+    payload = {
+        "persistence": True
+    }  # defining no services means all services will be cleared (we don't know which interservice communication tests trigger)
+    headers = {"content-type": "application/json"}
     url = "http://localhost:4566/_pods/state/reset"
-    requests.delete(
-        url,
-        json=payload,
-        headers=headers,
-        timeout=90
-    )
+    requests.delete(url, json=payload, headers=headers, timeout=90)
 
 
 @pytest.fixture(scope="function", autouse=True)
 def check_if_test_failed(request):
-    """ monitors the test status, e.g. if the test failed or not
-        if the test succeeded, write the collected metrics
-        into the raw-data-collection csv file
+    """monitors the test status, e.g. if the test failed or not
+    if the test succeeded, write the collected metrics
+    into the raw-data-collection csv file
     """
     cur_failed = request.session.testsfailed
     yield
@@ -108,10 +108,7 @@ def check_if_test_failed(request):
             pass
 
     url = "http://localhost:4566/metrics/reset"
-    r = requests.delete(
-        url,
-        timeout=90
-    )
+    r = requests.delete(url, timeout=90)
     assert r.status_code == 200
 
 
@@ -122,18 +119,22 @@ def pytest_addoption(parser):
         help="Comma separated list of services that should be tested",
     )
 
+
 def _startup_localstack():
     try:
         _localstack_health_check()
     except:
-        os.system('DNS_ADDRESS=127.0.0.1 EXTENSION_DEV_MODE=1 LOCALSTACK_API_KEY=$LOCALSTACK_API_KEY localstack start -d')
+        os.system(
+            "DNS_ADDRESS=127.0.0.1 EXTENSION_DEV_MODE=1 LOCALSTACK_API_KEY=$LOCALSTACK_API_KEY localstack start -d"
+        )
 
         _localstack_health_check()
 
     print("LocalStack running")
 
+
 def _shutdown_localstack():
-    os.system('localstack stop')
+    os.system("localstack stop")
 
 
 # TODO "package" doesn't seem to work (behaves like "session"), but "module" re-starts LS too often
@@ -147,21 +148,35 @@ def startup_localstack():
 
     _shutdown_localstack()
 
+
 @pytest.hookimpl()
 def pytest_sessionstart(session: "Session") -> None:
-    """ at the beginning of the test session: create the csv file where we will append the collected raw metrics """
+    """at the beginning of the test session: create the csv file where we will append the collected raw metrics"""
     Path(BASE_PATH).mkdir(parents=True, exist_ok=True)
     with open(FNAME_RAW_DATA_CSV, "w") as fd:
         writer = csv.writer(fd)
-        writer.writerow(["service", "operation", "parameters", "response_code", "response_data", "exception", "origin", "test_node_id"])
+        writer.writerow(
+            [
+                "service",
+                "operation",
+                "parameters",
+                "response_code",
+                "response_data",
+                "exception",
+                "origin",
+                "test_node_id",
+            ]
+        )
 
 
 def pytest_collection_modifyitems(items, config):
-    """ collects the selected tests depending on the services selected.
+    """collects the selected tests depending on the services selected.
     by default all services that LocalStack implement will be considered.
     With the option "--service=acm,lambda" a subset of services can be selected
     """
-    selected_services = config.option.services.split(",") if config.option.services else None
+    selected_services = (
+        config.option.services.split(",") if config.option.services else None
+    )
 
     selected_items = []
     deselected_items = []
@@ -169,7 +184,9 @@ def pytest_collection_modifyitems(items, config):
     if not selected_services:
         # no default, select all
         _startup_localstack()
-        response = requests.get("http://localhost:4566/_localstack/health").content.decode("utf-8")
+        response = requests.get(
+            "http://localhost:4566/_localstack/health"
+        ).content.decode("utf-8")
         selected_services = [k for k in json.loads(response).get("services").keys()]
 
     # TODO excluding EKS because it requires a lot of resources
@@ -194,7 +211,10 @@ def pytest_collection_modifyitems(items, config):
                 deselected_items.append(item)
             elif any([True for x in excluded_test_cases if x in item._nodeid]):
                 deselected_items.append(item)
-            elif f'test_{service}' in test_class_name or f'test_{service.replace("-", "")}' in test_class_name:
+            elif (
+                f"test_{service}" in test_class_name
+                or f'test_{service.replace("-", "")}' in test_class_name
+            ):
                 selected_items.append(item)
         if item not in selected_items:
             deselected_items.append(item)
@@ -213,4 +233,3 @@ def _localstack_health_check():
     session.mount("https://", adapter)
     session.get(localstack_health_url)
     session.close()
-
